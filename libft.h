0439ac39fabdcb6909ead172fa5ff079a9608b47
EidenAndreu <107546516+EidenAndreu@users.noreply.github.com>
2023-05-15 11:46:58 +0200

    ft_split con error de norminette

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ereinald <ereinald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:33:42 by ereinald          #+#    #+#             */
-/*   Updated: 2023/05/13 22:38:51 by ereinald         ###   ########.fr       */
+/*   Updated: 2023/05/15 11:13:32 by ereinald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ void		*ft_calloc(size_t count, size_t size);
 char		*ft_substr(const char *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
 
 #endif
