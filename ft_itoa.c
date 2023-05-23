cd47812d6be8b249a0d4e86fbc9eb69d605c5f29
EidenAndreu <107546516+EidenAndreu@users.noreply.github.com>
2023-05-23 21:35:33 +0200

    itoa corregido

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,56 +1,46 @@
 #include "libft.h"
-#include <limits.h>
 
-static int	ft_digit_count(long int i)
+static int	ft_digit_count(long n)
 {
 	int	count;
 
 	count = 0;
-	if (i < 0)
+	if (n <= 0)
 	{
-		i *= -1;
-		count++;
+		count = 1;
+		n = -n;
 	}
-	while (i > 0)
+	while (n > 0)
 	{
-		i /= 10;
 		count++;
+		n /= 10;
 	}
 	return (count);
 }
 
 char	*ft_itoa(int n)
 {
-	char		*str;
-	int			i;
+	char	*str;
+	int		i;
+	long	num;
 
-/*	if (n == INT_MIN)
-	{
-		str = (char *)malloc(12 * sizeof(char));
-		if (!str)
-			return (NULL);
-		ft_strlcpy(str, "-2147483648", 12);
-		return (str);
-	}
-*/	i = ft_digit_count(n);
-	str = malloc(i * sizeof(char) + 1);
+	num = n;
+	i = ft_digit_count(num);
+	str = (char *)malloc((i + 1) * sizeof(char));
 	if (!str)
-		return (0);
-	str[i--] = 0;
-	if (n == 0)
-	{
-		str = ft_calloc(2, sizeof(char));
-		str[0] = 48;
-	}
-	if (n < 0)
+		return (NULL);
+	str[i--] = '\0';
+	if (num == 0)
+		str[0] = '0';
+	if (num < 0)
 	{
 		str[0] = '-';
-		n = n * -1;
+		num = -num;
 	}
-	while (n > 0)
+	while (num > 0)
 	{
-		str[i--] = n % 10 + '0';
-		n = n / 10;
+		str[i--] = (num % 10) + '0';
+		num /= 10;
 	}
 	return (str);
 }
